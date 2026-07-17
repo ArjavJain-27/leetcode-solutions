709e6afeea6ea81917b267d4cff1119296ab06a4
Arjav Jain <arjav9191@gmail.com>
2026-07-17 22:22:41 +0530

    Time: 45 ms (70.64%), Space: 19.3 MB (18.37%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,7 +1,14 @@
 class Solution:
     def fib(self, n: int) -> int:
-
         if n <= 1:
             return n
 
-        return self.fib(n - 1) + self.fib(n - 2)
+        first = 0
+        second = 1
+
+        for i in range(2, n + 1):
+            third = first + second
+            first = second
+            second = third
+
+        return second
