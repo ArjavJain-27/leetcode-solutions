24d5456d71d0664785f5a6142f512c06fd3cc27b
Arjav Jain <arjav9191@gmail.com>
2026-07-23 22:48:27 +0530

    Time: 4 ms (60.25%), Space: 20.4 MB (61.47%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,16 +1,8 @@
 class Solution:
     def moveZeroes(self, nums):
-        temp = []
+        i = 0
 
-        # Store non-zero elements
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                temp.append(nums[i])
-
-        # Copy back
-        for i in range(len(temp)):
-            nums[i] = temp[i]
-
-        # Fill remaining with zeros
-        for i in range(len(temp), len(nums)):
-            nums[i] = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
