7cdc65d700f4bd5cdb85c5af2b1561569ed6bf2a
Arjav Jain <arjav9191@gmail.com>
2026-07-30 23:50:54 +0530

    Time: 5 ms (49.50%) | Memory: 20.5 MB (23.54%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,7 @@
+class Solution:
+    def moveZeroes(self, nums):
+        i = 0
+        for j in range(len(nums)):
+            if nums[j]!=0:
+                nums[j],nums[i]=nums[i],nums[j]
+                i+=1
